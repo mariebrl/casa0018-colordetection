45087898b1f88ed7d6d38d31fb2368960f957f5b
mariebrl <mariebourel@yahoo.com>
2023-04-27 16:10:17 +0100

    feat: last changes

--- arduino/arduino_colordetec.ino
+++ arduino/arduino_colordetec.ino
@@ -1,16 +1,3 @@
-#include <Arduino_APDS9960.h>
-
-#include <TensorFlowLite.h>
-
-// including our model in a seperate file to keep the code simpler
-#include "model.h"
-// using a variety of functions from the TensorFlowLite library and need to reference them here
-#include "tensorflow/lite/micro/all_ops_resolver.h"
-#include "tensorflow/lite/c/common.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/schema/schema_generated.h"
-#include "tensorflow/lite/version.h"
 /*
   Object classifier by color
   --------------------------
@@ -20,56 +7,80 @@
 
   Hardware: Arduino Nano 33 BLE Sense board.
 
-
-#include <Wire.h>
-#include <Adafruit_TCS34725.h>
-#include <Adafruit_GFX.h>
-//#include <Adafruit_SSD1306.h>
-
-
-#define OLED_RESET 4
-//Adafruit_SSD1306 display(OLED_RESET);
-
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_600MS, TCS34725_GAIN_1X);
 */
 
+#include <Arduino_APDS9960.h>
+#include <TensorFlowLite.h>
+#include <ArduinoBLE.h>
+
+// including our model in a seperate file to keep the code simpler
+#include "model.h"
+#include "/Users/mariebourel/Documents/Fac/Master 2022_2023/UCL/cours/CASA0018 - Deep Learning for sensor network /Coursework/arduino_ide/arduino_colordetec/model.cc"
+
+
+// using a variety of functions from the TensorFlowLite library and need to reference them here
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/c/common.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+
+// LED on the board
 #define RED 22
+#define GREEN 23
+#define BLUE 24
 
 // global variables used for TensorFlow Lite (Micro)
 // Define the variables used by TFLite (pointers)
 tflite::ErrorReporter* error_reporter = nullptr;
 const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
+//tflite::MicroInterpreter* interpreter = nullptr;
+
+tflite::MicroInterpreter* tflInterpreter = nullptr;
+
 TfLiteTensor* input = nullptr;
 TfLiteTensor* output = nullptr;
+char *result;
+float max_proba;
 
 // Create a static memory buffer for TFLM, the size may need to
-const int kTensorArenaSize = (2 * 1024);
+const int kTensorArenaSize = (12 * 1024);
 uint8_t tensor_arena[kTensorArenaSize];
 
-// This constant represents the range of x values our model was trained on
-const float kXrange = 255.f;
-
 // This constant determines number of inferences to perform across range of x values defined above. 
-const int kInferencesPerCycle = 8000;
+const int kInferencesPerCycle = 4;
 
 // A counter to keep track of how many inferences we have performed.
 int inference_count = 0;
 
-// array to map gesture index to a name
-const char *COLOURS[] = {"BLACK", "BLUE", "BROWN", "GREEN", "GREY", "ORANGE", "PINK", "PURPLE", "RED", "WHITE", "YELLOW"};
+// array to map color index to a label
+//const char *COLOURS[] = {"BLACK", "BLUE", "BROWN", "GREEN", "GREY", "ORANGE", "PINK", "PURPLE", "RED", "WHITE", "YELLOW"};
+char *COLOURS[] = {"BLACK", "BLUE", "BROWN", "GREEN", "GREY", "ORANGE", "PINK", "PURPLE", "RED", "WHITE", "YELLOW"};
 #define NUM_CLASSES (sizeof(COLOURS) / sizeof(COLOURS[0]))
 
 // TF_LITE_REPORT_ERROR
 #define TF_LITE_REPORT_ERROR
 
-// compteur loop
-int compteur = 3;
+// initialize the color values
+int r, g, b, c;
 
+// BLE create service and characteristics
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+
+BLEService colordetection_service("280B");
+//BLEUnsignedCharCharacteristic colordetectionChar("2101", BLERead | BLENotify);
+//BLECharCharacteristic colordetectio_Char(deviceServiceCharacteristicUuid, BLERead | BLENotify | BLEWrite);
+BLEStringCharacteristic colordetectio_Char(deviceServiceCharacteristicUuid, BLERead | BLENotify | BLEWrite, 7);
+//BLEStringCharacteristic colordetectio_Char("2A19", BLERead | BLENotify | BLEWrite, 7);
 /*
  * Initialise the sketch - all this code is run once at start-up
  */
 void setup() {
+  Serial.begin(9600);
+  while (!Serial){};
+
   Serial.println("Object classification using RGB color sensor");
   Serial.println("--------------------------------------------");
   Serial.println("Arduino Nano 33 BLE Sense running TensorFlow Lite Micro");
@@ -77,49 +88,33 @@ void setup() {
 
   if (!APDS.begin()) {
     Serial.println("Error initializing APDS9960 sensor.");
+    while (!APDS.begin()) {
+      Serial.print(F("."));
+      delay(500);
+    }
+  }
+
+  if (!BLE.begin()) {
+    Serial.println("- Starting Bluetooth® Low Energy module failed!");
+    while (1);
   }
 
   // initialize the digital Pin as an output
   pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
 
-  // initialize the pushbutton as an input:
-  const int buttonPin = 2; 
-
- // check if a color reading is available
-  while (! APDS.colorAvailable()) {
-    delay(5);
-  }
-  int r, g, b;
-  // read the state of the pushbutton value:
-  int buttonState = 0;
-  buttonState = digitalRead(buttonPin);
-  
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == 0) {
-    // read the color
-    APDS.readColor(r, g, b);
-    // print the values
-    Serial.print("Red light = ");
-    Serial.println(r);
-    Serial.print("Green light = ");
-    Serial.println(g);
-    Serial.print("Blue light = ");
-    Serial.println(b);
-    Serial.println();
-  
-  } else {
-    // turn LED off:
-    digitalWrite(RED, HIGH);
-  }  
   // Initialize LED ON
   digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
 
   // Set up a tflite mirco error reporter to allow us to log / display data to the terminal. 
   static tflite::MicroErrorReporter micro_error_reporter;
   error_reporter = &micro_error_reporter;
 
   // Load in the model we have defined in the model.cpp file and map it into a usable data structure. 
-  model = tflite::GetModel(g_model);
+  model = tflite::GetModel(models_model_no_quant_tflite);
 
   // Check to see if the model is valid using the version function in the library and 
   // if not valid then break out of setup since the code probably will not work!
@@ -131,140 +126,144 @@ void setup() {
     return;
   }
 
+  // Initialize Arduino BLE (service and characteristic)
+  BLE.setLocalName("ColorDetect");
+  BLE.setAdvertisedService(colordetection_service);
+  colordetection_service.addCharacteristic(colordetectio_Char);
+  
+  // add the service and set a value for the characteristic:
+  BLE.addService(colordetection_service);
+  //colordetectio_Char.writeValue('ff');
+  // start advertising
+  BLE.advertise();
+  
+  Serial.println("Bluetooth device active, waiting for connections...");
+
   // This pulls in all the operation implementations needed
   static tflite::AllOpsResolver resolver;
 
-
   // Build an interpreter to run the model with and create a variable that points to the address of that interpreter.
-  static tflite::MicroInterpreter static_interpreter(
-      model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
-  interpreter = &static_interpreter;
+  static tflite::MicroInterpreter static_interpreter(model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  tflInterpreter = &static_interpreter;
 
 
-    // Allocate memory from the tensor_arena for the model's tensors.
-  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  // Allocate memory from the tensor_arena for the model's tensors.
+  TfLiteStatus allocate_status = tflInterpreter->AllocateTensors();
   if (allocate_status != kTfLiteOk) {
     TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
     return;
   }
 
+  // Create an interpreter to run the model
+  //tflInterpreter = new tflite::MicroInterpreter(model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+
   // Obtain pointers to the model's input and output tensors. 
   // We have one tensor so our input and output reference item 0.
-  input = interpreter->input(0);
-  output = interpreter->output(0);
-
-  // initialise inference counter to 0.
-  inference_count = 0;
-  
+  input = tflInterpreter->input(0);
+  output = tflInterpreter->output(0);
 }
 
 void loop() {
-  //while(compteur > 0){
-
   /*
-   * The Loop has five main steps:
-   *  - work out an input x value
-   *  - convert that input value to be the same as the model type
-   *  - invoke the interpreter to work out an output given an input
-   *  - convert the output back into one of the color classes
-   */
-
-  int r, g, b;
-  int sum;
-  
-  /*   
-  *  Step 1: work out an input x value 
+  * The Loop has five main steps:
+  *  - work out an input x value
+  *  - convert that input value to be the same as the model type
+  *  - invoke the interpreter to work out an output given an input
+  *  - convert the output back into one of the color classes
   */
-  // Calculate an x value to feed into the model. 
-  // Position is a number between 0 and 1 depending on how far through the
-  // inference count cycle we are.
-  // We then mulitply that by our range factor to map it between 0 and 2PI.
-  // (ie the range of possible x values the model was trained on)
-  float position = static_cast<float>(inference_count) /
-                   static_cast<float>(kInferencesPerCycle);
-  float x_val = position * kXrange;
 
-  // check if both color and proximity data is available to sample
-  //while (!APDS.colorAvailable() || !APDS.proximityAvailable()) {
-    // read the color and proximity sensor
-  //  APDS.readColor(r, g, b);
-  //  sum = r + g + b;
-  //}
-  // read the color
-  APDS.readColor(r, g, b);
-  // print the values
-  Serial.print("Red light = ");
-  Serial.println(r);
-  Serial.print("Green light = ");
-  Serial.println(g);
-  Serial.print("Blue light = ");
-  Serial.println(b);
-  Serial.println();
+  /*  Step 1: work out an input x value */
+  // Calculate input values to feed into the model. 
+  while(APDS.colorAvailable()){
+    // read the color
+    APDS.readColor(r, g, b, c);
+    // print the values
+    Serial.print("Red light = ");
+    Serial.println(r);
+    Serial.print("Green light = ");
+    Serial.println(g);
+    Serial.print("Blue light = ");
+    Serial.println(b);
+    //Serial.print("Light = ");
+    //Serial.println(c);
+    Serial.print("\n");
 
-  /*
-  *  Step 2: convert that input value to be the same as the model type (quantized)
-  */  
-  // check if there's an object close and well illuminated enough
-  if (sum > 0) {
+    double sum = r + g + b;
 
-    float redRatio = r / 255;
-    float greenRatio = g / 255;
-    float blueRatio = b / 255;
-
-    // input sensor data to model
+    double redRatio = r / sum;
+    double greenRatio = g / sum;
+    double blueRatio = b / sum;
+    
+    /*  Step 2: convert that input value to be the same as the model type */
+    // check if there's an object close and well illuminated enough
     input->data.f[0] = redRatio;
     input->data.f[1] = greenRatio;
     input->data.f[2] = blueRatio;
 
-    /*  
-    *  Step 3: invoke the interpreter to work out an output given an input 
-    */
+    /*  Step 3: invoke the interpreter to work out an output given an input */
+    TfLiteTensor* output = tflInterpreter->output(0);
     // Run inferencing
-    TfLiteStatus invoke_status = interpreter->Invoke();
-    
-    // Check to see if invoke went ok                       
-    if (invoke_status != kTfLiteOk) {
-      TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed on x_val: %f\n",
-                          static_cast<double>(x_val));   
-      return;
-    }
+    TfLiteStatus invoke_status = tflInterpreter->Invoke();
 
-    /* 
-    *  Step 4: convert the output back into a number between -1 and 1
-    */
-    // Output results
-    for (int i = 0; i < 10; i++) {
+    /*  Step 4: convert the output back into a color label */
+    // Output results 
+    /*
+    for (int i = 0; i < 11; i++) {
+      Serial.print("result output ");
+      Serial.print("\n");
       Serial.print(COLOURS[i]);
-      Serial.print(" ");
-      Serial.print(int(output->data.f[i] * 100));
+      Serial.print(": ");
+      Serial.print(output->data.f[i]*100, 6);
       Serial.print("%\n");
     }
-    Serial.println();
+    */
+
+    // Print the max probability
+    max_proba = output->data.f[0];
+    for (int i = 0; i < 11; i++) {
+      max_proba = max(output->data.f[i], max_proba);
+    }
+    for (int i = 0; i < 11; i++) {
+      if (output->data.f[i] == max_proba){
+        Serial.print("Colour Label: \n");
+        result = COLOURS[i];
+        Serial.print(COLOURS[i]);
+        Serial.print("\n");
+      }
+    }
+    
+    // Display result via BLE
+    BLEDevice central = BLE.central();
+    Serial.println("- Discovering central device...");
+    delay(500);
+
+    if (central) {
+      Serial.print("Connected to central: ");
+      Serial.println(central.address());
+
+      while (central.connected()) {
+        //const unsigned char color = (char*) result ;
+        Serial.print("Color Class: ");
+        Serial.println(max_proba);
+        Serial.println(result);
+        //colordetectio_Char.writeValue(*result);
+        colordetectio_Char.setValue(result);
+        if (colordetectio_Char.subscribed()) {
+          // set a new value , that well be pushed to subscribed Bluetooth® Low Energy devices
+          //colordetectio_Char.writeValue(*result);
+        }
+        delay(15000);
+      }
+    }
   }
 
-  // Increment the inference_counter, and reset it if we have reached
-  // the total number per cycle
-  inference_count += 1;
-  if (inference_count >= kInferencesPerCycle) inference_count = 0;
-
-  /* 
-    *  Step 4: convert the output back into a number between -1 and 1
-    */
-    // Output results
-  for (int i = 0; i < 10; i++) {
-      Serial.print(COLOURS[i]);
-      Serial.print(" ");
-      Serial.print(int(output->data.f[i] * 100));
-      Serial.print("%\n");
-      Serial.print(" ");
-    }
   
-  Serial.print(" ");
-  compteur = compteur - 1;
+  delay(15000);
   
+  Serial.print("Disconnected from central: ");
+  //Serial.println(central.address());
 
-  // Add ddelay
-  delay(5000);
+  delay(15000); // wait 10 sec
 }
 
 
--- arduino/model.h
+++ arduino/model.h
@@ -3,10 +3,10 @@
 // C data array, so it can be easily compiled into a binary for devices that
 // don't have a file system.
 
-#ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_MODEL_H_
-#define TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_MODEL_H_
+#ifdef TENSORFLOW_LITE_MICRO_COLOR_DETECT_MODEL_H_
+#define TENSORFLOW_LITE_MICRO_COLOR_DETECT_MODEL_H_
 
-extern const unsigned char g_model[];
-extern const int g_model_len;
+extern const unsigned char models_model_no_quant_tflite[];
+extern const int models_model_no_quant_tflite_len;
 
-#endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_MODEL_H_
+#endif  // TENSORFLOW_LITE_MICRO_COLOR_DETECT_MODEL_H_
